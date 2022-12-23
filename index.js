0cbe4e24cf2bfe13d5007e5bf68fcaa858163542
Caitlin Jordan <jordan.cait17@gmail.com>
2022-12-22 18:06:50 -0800

    completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x, y){
+    return x + y;
+}
+add()
+function subtract(x, y){
+    return x - y;
+}
+subtract()
+function multiply(x, y){
+    return x * y;
+}
+multiply()
+function divide(x, y){
+    return x / y;
+}
+divide()
+function increment(x){
+    return x + 1;
+}
+increment()
+function decrement(y){
+    return y - 1;
+}
+decrement()
+function makeInt(x){
+    return parseFloat(x);
+}
+makeInt()
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+preserveDecimal()
